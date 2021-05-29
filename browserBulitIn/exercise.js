716da9b873f2517d9525d69d12a0c486dd27158a
dusanstankovic84 <duxbih@outlook.com>
2021-05-29 16:04:25 +0200

    BulitIn

--- browserBulitIn/exercise.js
+++ browserBulitIn/exercise.js
@@ -4,6 +4,10 @@
         console.log("Dusan Stankovic");
 })();
 
+
+// window.navigator
+
+
 function winNav() {
     console.log(window.navigator.platform);
     console.log(window.navigator.appVersion);
@@ -20,6 +24,9 @@ function isOnline() {
 }
 isOnline()
 
+
+// window.screen
+
 function winScreen() {
     console.log(window.screen.availHeight);
     console.log(window.screen.availWidth);
@@ -28,13 +35,15 @@ function winScreen() {
 winScreen()
 
 
-function winLoc() {
+// window.location
+
+function winLocation() {
     console.log(window.location.href);
     console.log(window.location.host);
     console.log(window.location.protocol);
     console.log(window.location.search);
 }
-winLoc()
+winLocation()
 
 function winReload() {
     window.location.reload;
@@ -43,6 +52,39 @@ function winReload() {
 function redirectToWebsite(){
     window.location.replace("https://www.wikipedia.org/")
 }
+
+
+// window.localStorage
+
+function storesData() {
+    window.localStorage.setItem("lastName", "Stankovic");
+    window.localStorage.setItem("firstName", "Dusan");
+}
+
+
+function readData() {
+    var data = localStorage.getItem("firstName");
+    if (!data) {
+        return "there is no data";
+    } else {
+        return data;
+    }
+}
+console.log(readData());
+
+function removeData () {
+    lacalStorage.removeItem("firstName")
+}
+
+console.log(removeData());
+
+
+// window.history
+
+
+
+
+
 
 
 
