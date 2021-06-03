7c0edd0c22473467455c2676e07fadd3ba294dab
dusanstankovic84 <duxbih@outlook.com>
2021-06-03 17:57:10 +0200

    objects

--- browserBulitIn/exercise.js
+++ browserBulitIn/exercise.js
@@ -79,7 +79,84 @@ function removeData () {
 console.log(removeData());
 
 
+
+// sessionStorage
+
+function sessionData() {
+    window.sessionStorage.setItem("Pet", "Cat");
+}
+sessionData();
+  
+  
+function setSesName() {
+    window.sessionStorage.setItem("Name", "Dusan");
+    window.sessionStorage.setItem("Age", "36");
+}
+setSesName();
+
+  
+function getSesName() {
+    console.log(window.sessionStorage.getItem("Name"));
+}
+getSesName();
+
+  
+function removeSessInfo(a) {
+    window.sessionStorage.removeItem("Name");
+}
+removeSessInfo("Name");
+
+
+function getSessName() {
+    if (window.sessionStorage.getItem("Name")) {
+      console.log(window.sessionStorage.getItem("Name"));
+    } else {
+      console.log("There is no data");
+    }
+}
+getSessName()
+
+
+
 // window.history
+
+function goBack() {
+    window.history.go(-2);
+}
+goBack();
+  
+  
+window.history.forward();
+window.history.back();
+
+
+
+
+// Window Methods
+
+function submitAnswer() {
+    alert("Hello dear user!");
+  
+    var userAnswer = prompt("Please enter your name:");
+    var getUserAnswerLength = userAnswer.length;
+  
+    //Making sure that prompt() can't be empty
+    if (getUserAnswerLength > 0) {
+      userAnswer;
+    } else if (getUserAnswerLength === 0) {
+      submitAnswer();
+    }
+    //
+  
+    var userConfirmation = confirm(
+      "We will submit this answer now!" + "\nYour answer: " + userAnswer
+    );
+    if (userConfirmation === true) {
+      alert("You have successfully submitted the answer.");
+    }
+}
+  
+submitAnswer();
 
 
 
